6540c5300125290950f9d69918a19e52c2bd4f9f
Sammion <samqian163@163.com>
2017-12-26 20:47:43 +0800

    练习层次化索引

--- src/ch05/Test12.py
+++ src/ch05/Test12.py
@@ -3,7 +3,7 @@
 @date 2017-12-25
 @des 第五章pandas入门
 这里主要练习层次化索引。
-层次化索引是pandas的忆襄重要功能， 它是你能在一个轴上拥有多个索引级别。
+层次化索引是pandas的重要功能， 它是你能在一个轴上拥有多个索引级别。
 抽象点说，它使你能以低维度形式处理高维度数据。
 '''
 
@@ -36,8 +36,18 @@ frame = pd.DataFrame(np.random.randn(12).reshape(4, 3), index=[['a', 'a', 'b', '
                      columns=[['L', 'L', 'S'], ['C++', 'Python', 'Java']])
 print(frame)
 print('================>我就是分隔线 8 <==============')
-frame.index.names = ['key1','key2']
-frame.columns.names = ['Style','name']
+frame.index.names = ['key1', 'key2']
+frame.columns.names = ['Style', 'name']
 print(frame)
 print(frame['L'])
 print(frame.L)
+print('================>我就是分隔线 9 <==============')
+print(frame)
+frame.swaplevel('key1', 'key2')
+print(frame.swaplevel('key1', 'key2'))
+print('================>我就是分隔线 10 <==============')
+print(frame.swaplevel('key1', 'key2').sort_index(level='key2'))
+print('================>我就是分隔线 11 <==============')
+print(frame.sum(level='key2'))
+print(frame.sum(level='key1'))
+# print(frame.sum(level='L', axis=1))
