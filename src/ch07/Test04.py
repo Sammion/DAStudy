cdb309d976f02392b4fcbbba8a3c3d6c3d03604e
Sammion <samqian163@163.com>
2017-12-27 19:40:16 +0800

    练习去除重复数据

--- src/ch07/Test04.py
+++ src/ch07/Test04.py
@@ -23,5 +23,39 @@ data['v1'] = range(7)
 tmp = data.drop_duplicates(['k1'])
 print(tmp)
 print('================>我就是分隔线 7 <==============')
-tmp = data.drop_duplicates(['k1','k2'], keep='last')
+tmp = data.drop_duplicates(['k1', 'k2'], keep='last')
+print(tmp)
+print('================>我就是分隔线 8 <==============')
+data = pd.DataFrame({'food': ['bacon', 'pulled pork', 'bacon', 'Pastrami', 'corned beef', 'Bacon', 'pastrami',
+                              'honey ham', 'nova lox'],
+                     'ounces': [4, 3, 12, 6, 7.5, 8, 3, 5, 6]})
+print(data)
+
+meat_to_animal = {
+    'bacon': 'pig',
+    'pulled pork': 'pig',
+    'pastrami': 'cow',
+    'corned beef': 'cow',
+    'honey ham': 'pig',
+    'nova lox': 'salmon'
+}
+print('================>我就是分隔线 9 <==============')
+data['animal'] = data['food'].map(str.lower).map(meat_to_animal)
+print(data)
+print('================>我就是分隔线 10 <==============')
+data['animal'] = data['food'].map(lambda x: meat_to_animal[x.lower()])
+print(data)
+print('================>我就是分隔线 10 <==============')
+
+obj = pd.Series([1, -999, 2, 3, -999, -1000, 2])
+tmp = obj.replace(-999, np.nan)
+print(tmp)
+print('================>我就是分隔线 11 <==============')
+tmp = obj.replace([-999, 1], np.nan)
+print(tmp)
+print('================>我就是分隔线 12 <==============')
+tmp = obj.replace([-999, 1], [np.nan, 0])
+print(tmp)
+print('================>我就是分隔线 13 <==============')
+tmp = obj.replace({-999: np.nan, 2: 200.})
 print(tmp)
