7e00c79fa11328b8b7b1025111e5b8c4c3b5a5ef
Sammion <samqian163@163.com>
2018-01-02 18:39:33 +0800

    练习Matplotlib创建canvas

--- src/ch08/Test01.py
+++ src/ch08/Test01.py
@@ -2,7 +2,7 @@
 @author Sam
 @date 2018-01-02
 @des 第七章 数据可视化
-这里主要练习Matplotlib 
+这里主要练习Matplotlib创建canvas 
 Figure 和 Subplot
 '''
 
@@ -16,11 +16,11 @@ ax1 = fg.add_subplot(2, 2, 1)
 ax2 = fg.add_subplot(2, 2, 2)
 ax3 = fg.add_subplot(2, 2, 3)
 # 默认使用最后一个子图
-plt.plot(random(50), 'k--')
-ax1.hist(random(100), bins=20, color='k', alpha= 0.3 )
-ax2.scatter(np.arange(30), np.arange(30)+3*random(30))
+plt.plot(random(500), 'k--')
+ax1.hist(random(1000), bins=20, color='k', alpha=0.3)
+ax2.scatter(np.arange(30), np.arange(30) + 3 * random(30))
 # 同时创建figure和ax子图对象
-fig, axes = plt.subplot(2,3)
+# fig, axes = plt.subplot(2, 3)
 
 
 plt.show()
